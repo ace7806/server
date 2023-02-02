8c34053683083625abdaff948004f04a1b9ef8fa
Sebastian Rodriguez <red7806@gmail.com>
2023-02-02 17:47:20 -0400

    huge refactor

--- app/utils.py
+++ app/utils.py
@@ -1,13 +1,11 @@
 from geopy.distance import geodesic
-import json
-import re
 
-def calculate_distance_in_miles(lat1,long1,lat2,long2):
 
-    return geodesic((lat1,long1),(lat2,long2)).miles
+def calculate_distance_in_miles(lat1, long1, lat2, long2):
+    return geodesic((lat1, long1), (lat2, long2)).miles
+
 
 def serialize_gasStations(gasStations):
     geoJsons = [x.to_geoJson_dict() for x in gasStations]
     feature_collection = {"type": "FeatureCollection", "features": geoJsons}
     return feature_collection
-
