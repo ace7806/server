65df22df58bf2aec6896fdd9d5342c5fdf25d74b
Sebastian Rodriguez <red7806@gmail.com>
2023-02-02 17:48:21 -0400

    change

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,3 @@
-from geopy.distance import geodesic
-
-
-def calculate_distance_in_miles(lat1, long1, lat2, long2):
-    return geodesic((lat1, long1), (lat2, long2)).miles
-
-
 def serialize_gasStations(gasStations):
     geoJsons = [x.to_geoJson_dict() for x in gasStations]
     feature_collection = {"type": "FeatureCollection", "features": geoJsons}
