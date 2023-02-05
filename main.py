e348fb709c0f5f30f87302088029f3f26727ac8a
Sebastian Rodriguez <red7806@gmail.com>
2023-02-04 21:08:45 -0400

    changed port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000), debug=True)
