47874e014b3622775ee8f45493673cfd6f7a22a6
ace7806 <red7806@gmail.com>
2023-04-05 17:12:35 -0400

    rollback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from app import app
 import os
-from scripts import scheduler
+#from scripts import scheduler
 
 if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
