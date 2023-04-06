48fcfa9b02a5611de4ebc9a2e566120b335fd643
ace7806 <red7806@gmail.com>
2023-04-06 17:10:19 -0400

    update todays prices

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -1,11 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-# from update_gas_prices import update_todays_prices
+from update_gas_prices import update_todays_prices
 
 def gas_price_update_job():
-    # with app.app_context():
-    #update_todays_prices()
+    update_todays_prices()
     print('Gas price update job has been executed')
-#gas_price_update_job()
 scheduler = BackgroundScheduler()
 scheduler.add_job(gas_price_update_job, 'interval', 
                   seconds=1)
